435457cf7f677d16a450fda111e9f43b92280a48
Maimoon <maimoon.ideal@gmail.com>
2026-06-01 17:16:51 +0500

    feat: add console logging configuration to development settings

--- AI_Chat_Bot/config/settings/development.py
+++ AI_Chat_Bot/config/settings/development.py
@@ -33,3 +33,11 @@ if not SECRET_KEY:
 # Email backend – console for development
 # -------------------------------------------------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {'console': {'class': 'logging.StreamHandler'}},
+    'root': {'handlers': ['console'], 'level': 'INFO'},
+}
